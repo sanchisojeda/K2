8886a0775fa850b841b8f5c759f8180afd9298f2
Daniel Allepuz Requena <daniel.allepuz@gmail.com>
2015-07-06 16:38:36 -0700

    Added mag to evaluate output

--- evaluate.py
+++ evaluate.py
@@ -8,14 +8,14 @@ import scipy as sp
 targets = datalib.get_column(K2.read_target_file('K2Campaign0targets.csv - K2Campaign0targets.csv'),0)
 del targets[0]
 
-result = [['#EPIC','sigma_1','sigma_2','sigma_3','sigma_4','sigma_5']]
+result = [['#EPIC','mag','sigma_1','sigma_2','sigma_3','sigma_4','sigma_5']]
 errors = []
 for i in range(0,len(targets)):
 	target = targets[i]
 	print target + ' : ' + str(int((100.0*i)/len(targets))) + '%' 
 	info = K2.get_K2_info(int(target),poly_fit_deg = 8)
 	if info['error'] == 'None':
-		star = [target]
+		star = [target,info['star_info'][2]]
 		data_by_segs = info['subtraction_by_segments']
 		for seg in data_by_segs:
 			sigma = np.std(seg[1])
