59d0da87b8da072f17776b7e418dba24ef22b8c5
Daniel Allepuz Requena <daniel.allepuz@gmail.com>
2015-07-08 14:18:40 -0700

    Lot4

--- plot_mag_vs_sigma.py
+++ plot_mag_vs_sigma.py
@@ -1,7 +1,11 @@
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 import datalib as dl
+import astroML 
+from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=10, usetex=True)
 
 def color_T(Teff):
 	if Teff == 'nan' or Teff == '-1.0' or Teff == '1.0':
@@ -9,10 +13,16 @@ def color_T(Teff):
 	Teff = float(Teff)
 	x = (Teff-2265)/5375.0
 	return [-x*x+1,-4*x*(x-1),-x*(x-2)]
-
+def label_d(is_dwarf):
+	if is_dwarf == '1.0':
+		return 'Dwarf'
+	elif is_dwarf == '0.0':
+		symbol = 'Giant'
+	else:
+		symbol = 'Unkown'
 data = dl.read_dat('evaluation.csv',',')
 
-f,plots = plt.subplots(3,2, sharex=True,sharey=True)
+f,plots = plt.subplots(3,2, sharex=False,sharey=False)
 
 dwarf_flags = dl.get_column(data,1)
 kepmags = dl.get_column(data,3)
@@ -32,11 +42,18 @@ for i in range(1,length):
 		else:
 			symbol = 'x'
 
-		if sigma != 'nan' and kepmags[i] != 'nan': 
+		if sigma != 'nan' and kepmags[i] != 'nan':
 			plots[np.ceil((seg-4)/2)][(seg-4)%2].scatter(float(kepmags[i]),np.log10(float(sigmas[seg-4][i])),marker=symbol,color = color_T(Teffs[i]))
-			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_ylabel('$\log{\sigma_' + str(seg-3) +'}$')
-			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_xlabel('$Kepler \, band \, magnitude$')
-			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_title('$Segment \, '+ str(seg-3) + '$')
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_ylabel('$\log{\sigma_' + str(seg-3) +'}$',fontsize=20)
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].tick_params(axis='both', which='major', labelsize=20)
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].grid(True)
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_xlim([8.25,17])
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].set_ylim([-4.5,0])
+			plots[np.ceil((seg-4)/2)][(seg-4)%2].text(15.25,-0.5,'Segment ' + str(seg-3),fontsize=30)
+		print int((i*100)/length)
 
-		print int((i*100)/length) 
-plt.show()
+plots[2][0].set_xlabel('Kepler band magnitude',fontsize=20)
+plots[1][1].set_xlabel('Kepler band magnitude',fontsize=20)
+f.delaxes(plots[2][1])		
+f.set_size_inches(32,20)
+f.savefig("sigma_vs_mag.png", dpi = 300)
